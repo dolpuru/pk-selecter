58ce631bddccb87d6562cce6d899f1545ef35f81
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-10-09 12:35:29 +0900

    back

--- PKSelecter/app/api/views_api/__init__.py
+++ PKSelecter/app/api/views_api/__init__.py
@@ -1,9 +1,9 @@
 """
 기본 view를 return
 """
-from flask import Blueprint
-from flask import app
-def views_deco(func):
-    Blueprint('views_bp', __name__)
-    return func
+from flask import Blueprint, render_template
+from .views import main_page
 
+
+views_bp = Blueprint('views_bp', __name__)
+main_page(views_bp, render_template)
--- PKSelecter/app/api/views_api/views.py
+++ PKSelecter/app/api/views_api/views.py
@@ -1,7 +1,11 @@
 from flask import render_template, app
-from app.api.views_api import views_deco
+from app.api.views_api import views_bp
 
-@views_deco
-@app.route('/main')
-def index():
-    return render_template("index.html")
+def main_page(bp, render_template):
+    @bp.route('/main')
+    def index():
+        return render_template("index.html")
+# @views_deco
+# @app.route('/main')
+# def index():
+#     return render_template("index.html")
