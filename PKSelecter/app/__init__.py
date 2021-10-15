2a3234f731bafaaff35f9f956e9bf0c3b0110b56
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-10-15 16:57:55 +0900

    add gitignore, __pychache__

--- PKSelecter/app/__init__.py
+++ PKSelecter/app/__init__.py
@@ -16,7 +16,6 @@ def create_app(config):
     app = Flask(__name__, static_folder="./templates/", template_folder="./templates/")
     app.config.from_object(config)
     print(app.config["ENV"])
-    print(app.config["NAME"])
     app.register_blueprint(views_bp, url_prefix="/")
     app.register_blueprint(controller_bp, url_prefix="/")
     return app
