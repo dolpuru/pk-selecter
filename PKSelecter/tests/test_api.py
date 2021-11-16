7b8b08664a795b08c0cb1b97b95fde2d66a4e0dd
doongu <must1080@naver.com>
2021-11-16 21:35:51 +0900

    modify test commit

--- PKSelecter/tests/test_api.py
+++ PKSelecter/tests/test_api.py
@@ -1,10 +1,6 @@
 """
-test_login_checker Test Case
+test_api Test Case
 """
-# api에 테스트 해보기 위한 데이터 생성 부분입니다.
-# 어떤 필요한 데이터들을 생성해서 test를 해볼 것인가?를 주된 관점으로 작성해보았습니다.
-# 서버에 대한 API 요청을 테스트 해볼 것이기에, 이에 필요한 요소들을 정리해보겠습니다.
-
 import sys, os
 import json
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))) #app을 import 하기 위함
@@ -21,8 +17,6 @@ class MyTest(unittest.TestCase):
 
     def setUp(self):
         self.app = create_app(config_dict['testing'])
-        self.app_context = self.app.app_context()
-        self.app_context.push()
         self.client = self.app.test_client()
 
   
@@ -49,5 +43,5 @@ class MyTest(unittest.TestCase):
 
             self.assertEqual(format_resp['status'],  fake_data[index]['status_code'])
             print(">>>검증 완료<<< \n")
-    
+
     # def test_login_sucess(self):
--- PKSelecter/tests/test_selecter.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-
-class MyTest(unittest.TestCase):
-    def runTest(self):
-        self.assertEqual(1, 1)
